6f3d6fca5bd7feef4ea3d354d14346943dc93be6
messham <jm.messham@gmail.com>
2016-10-18 10:30:40 -0700

    c++ template

---
+++ c++template.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int foo (int bar) {
+  return 42;
+}
+
+int main() {
+  int tmp[] = {0, 0, 1};
+  vector<int> nums {0, 0, 1};
+  vector<int> result;
+
+  return 0;
+}
