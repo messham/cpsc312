5c1c9f5b437d83074cbb7b42ce9326450bc4b922
lelbarton <lauraelbarton@gmail.com>
2016-10-22 13:48:22 -0700

    If there are any lines without semicolon delimiter, add them to the output list

--- SyntaxValidator/syntax.pl
+++ SyntaxValidator/syntax.pl
@@ -6,12 +6,18 @@ number_line([H|T], Num, [LH|LT]) :-
     NPlus is Num + 1,
     number_line(T, NPlus, LT).
 
-% true if the given string Line ends with a semicolon
-validate([]).
+% Adds a line to the ProblemList if it is not ended by a semicolon
+% TODO add only a line number
+check_code([],[]).
+check_code([H|T], [H|ProblemList]) :-
+    \+ validate(H),
+    check_code(T, ProblemList).
+check_code([_|T], ProblemList) :-
+    check_code(T, ProblemList).
 
-validate([Line|T]) :-
-    semicolon_delimited(Line),
-    validate(T).
+% true if the given string Line ends with a semicolon
+validate(Line) :-
+    semicolon_delimited(Line).
 % TODO check include packages lines
 % TODO check that brackets are balanced
 
@@ -21,7 +27,7 @@ semicolon_delimited(Line) :-
 % checks for the beginning and end of a function or block
 % TODO this is pretty naive, does not balance brackets
 % only recognizes that they are a valid alternative to semicolons
-block_begin(Line) :-
-    string_concat(_, "{", Line).
-block_end(Line) :-
-    string_concat(_, "}", Line).
+% block_begin(Line) :-
+%     string_concat(_, "{", Line).
+% block_end(Line) :-
+%     string_concat(_, "}", Line).
